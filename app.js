02fa74145ea7d59cb1ae6a253aa19da83f97c23c
Alex <websites@hello-24.com>
2025-08-08 18:48:28 -0500

    MVC part 1

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ app.set('view engine', 'ejs');
 app.set('views', 'views')
 //app.set('views', path.join(__dirname, 'views')); // safer than string
 //app.locals.basedir = path.join(__dirname, 'views');
-//
-const adminData = require('./routes/admin');
+
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const errorController = require('./controllers/error');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// 404 page (now rendered from Pug)
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+
+app.use(errorController.get404);
 
 app.listen(3000);
---
+++ controllers/products.js
@@ -0,0 +1,34 @@
+// Import class
+
+const Product = require('../models/product')
+
+
+// GET /admin/add-product
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
+// POST /admin/add-product
+exports.postAddProduct = (req, res, next) => {
+const product = new Product(req.body.title);
+product.save();
+res.redirect('/');
+};
+
+// GET /
+exports.getProducts = (req, res, next) => {
+const products = Product.fetchAll((products) => {
+    res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/'
+  });
+});
+  
+};
